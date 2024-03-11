341d3dfe744d786cddd4a84745c25ab386eaed38
Alejandra Paillas <ale@ventas5.>
2024-03-11 13:48:35 -0300

    hello {name}

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/hello/<name>', methods=['GET'])
+def hello_name(name):
+    return f'Hello {name}'
